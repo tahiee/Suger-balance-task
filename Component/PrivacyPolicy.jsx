033fe3b49053682d2201113d6ea1f3675c4f759f
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-31 22:46:36 +0500

    initilaze again

--- Component/PrivacyPolicy.jsx
+++ Component/PrivacyPolicy.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import img1 from "../Images/privacy.jpg";
+import img2 from "../Images/globel shipping.webp";
+import img3 from "../Images/panpapper.jpg";
 const PrivacyPolicy = () => {
   const policies = [
     {
-      imgSrc: '../Images/privacy.jpg', // Replace with the actual image path
-      title: 'Private Policy',
-      link: '#',
+      imgSrc: img1, // Replace with the actual image path
+      title: "Private Policy",
+      link: "#",
     },
     {
-      imgSrc: '../Images/globel shipping.webp', // Replace with the actual image path
-      title: 'Private Policy',
-      link: '#',
+      imgSrc: img2, // Replace with the actual image path
+      title: "Private Policy",
+      link: "#",
     },
     {
-      imgSrc: '../Images/panpapper.jpg', // Replace with the actual image path
-      title: 'Private Policy',
-      link: '#',
+      imgSrc: img3 , // Replace with the actual image path
+      title: "Private Policy",
+      link: "#",
     },
   ];
 
@@ -25,9 +27,16 @@ const PrivacyPolicy = () => {
       <div className="policyCards">
         {policies.map((policy, index) => (
           <div className="policyCard" key={index}>
-            <img src={policy.imgSrc} alt={policy.title} width='600px' height='160px' />
+            <img
+              src={policy.imgSrc}
+              alt={policy.title}
+              width="600px"
+              height="160px"
+            />
             <h3>{policy.title}</h3>
-            <a href={policy.link} className="readMoreBtn">READ MORE</a>
+            <a href={policy.link} className="readMoreBtn">
+              READ MORE
+            </a>
           </div>
         ))}
       </div>
