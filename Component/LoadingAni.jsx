32b96c8372039c78a4340d99270fa35d3d2ec104
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-31 22:51:35 +0500

    loading

---
+++ Component/LoadingAni.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RouterConfig from "../router/RouterConfig";
+import LoadingAnimation from "../Component/LoadingAni";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Set loading to false after 2 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <RouterConfig />
+      {isLoading ? (
+        <LoadingAnimation/> // Render loading animation while isLoading is true
+      ) : (
+        <RouterConfig /> // Render RouterConfig when isLoading is false
+      )}
     </>
   );
 };
