0e260e50d6d488de967600138148df1844c788a0
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-31 23:26:25 +0500

    res

--- Component/Header.jsx
+++ Component/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
               <h1>Sugar Balance</h1>
             </div>
             <div className="both-logo">
-              <div className="svg-container blink">
+              <div className="svg-container1 blink">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -32,7 +32,7 @@ const Header = () => {
                   <path d="M12 15.5l-6-4.5v9l6 4.5 6-4.5v-9l-6 4.5zm0-13L5.33 7.5 12 12.5l6.67-5L12 2.5z" />
                 </svg>
               </div>
-              <div className="svg-container">
+              <div className="svg-container2">
                 <img src={img2} alt="" className="png-logo-globel" />
               </div>
             </div>
